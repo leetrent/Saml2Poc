1c8ff9d952dad7d729040200375a4153265c506e
LeeTrent <lee.trent.1@gmail.com>
2022-12-05 08:53:46 -0600

    First commit, seeding project

---
+++ Saml2Poc/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Saml2Poc.Controllers
+{
+    [AllowAnonymous]
+    [Route("auth")]
+    public class AuthController : Controller
+    {
+        [Route("login")]
+        public ActionResult Login()
+        {
+            return RedirectToAction("Page2", "Home");
+        }
+    }
+}
---
+++ Saml2Poc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Saml2Poc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

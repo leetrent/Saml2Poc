7a29cb672ec23e39b5456646a9376952751573c5
LeeTrent <lee.trent.1@gmail.com>
2022-12-06 06:34:10 -0600

    Got things working with Okta (bare bones)

--- Saml2Poc/Controllers/AuthController.cs
+++ Saml2Poc/Controllers/AuthController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Saml2Poc.Utils;
+using System.Security.Authentication;
+using System.Text;
 
 namespace Saml2Poc.Controllers
 {
@@ -9,6 +12,21 @@ namespace Saml2Poc.Controllers
     {
         [Route("login")]
         public ActionResult Login()
+        {
+            string logSnippet = new StringBuilder("[")
+                                .Append(DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss"))
+                                .Append("][AuthController][Login] => ")
+                                .ToString();
+            string assertionConsumerServiceUrl = $"https://{Request.Host}{Request.PathBase}/{Saml2Constants.ASSERTION_CONSUMER_SERVICE_URI}";
+            Console.WriteLine(logSnippet + $"(assertionConsumerServiceUrl): '{assertionConsumerServiceUrl}'");
+
+            string url = "https://trial-1098158.okta.com/app/trial-1098158_saml2poc_1/exk3ie3sb1xK554EB697/sso/saml";
+
+            return Redirect(url);
+        }
+
+        [Route("AssertionConsumerService")]
+        public ActionResult AssertionConsumerService()
         {
             return RedirectToAction("Page2", "Home");
         }
---
+++ Saml2Poc/Utils/Saml2Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace Saml2Poc.Utils
+{
+    public class Saml2Constants
+    {
+        public static readonly string ASSERTION_CONSUMER_SERVICE_URI = "Auth/AssertionConsumerService";
+    }
+}
